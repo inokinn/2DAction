0b67e4053b65d4390cd7d4a147c5aa17473e19bc
inokinn <inokin1987@gmail.com>
2021-07-24 23:28:29 +0900

    カメラの調整 & ステージクリア機能の実装

--- Assets/Scripts/Presentation/View/Stage/CameraView.cs
+++ Assets/Scripts/Presentation/View/Stage/CameraView.cs
@@ -11,15 +11,31 @@ public class CameraView : MonoBehaviour
     // プレイヤーのゲームオブジェクト
     [SerializeField] private GameObject _player;
 
+    // プレイヤーのスクリプト
+    private PlayerPhysics _playerPhisics;
+    // カメラ
+    private Camera _camera;
+
     // Start is called before the first frame update
     void Start()
     {
-        gameObject.transform.position = new Vector3(_player.transform.position.x, _player.transform.position.y + 2, _player.transform.position.z - _CameraDistance);
+        _playerPhisics = _player.GetComponent<PlayerPhysics>();
+        _camera = gameObject.GetComponent<Camera>();
+        gameObject.transform.position = new Vector3(_player.transform.position.x, _player.transform.position.y + 2f, _player.transform.position.z - _CameraDistance);
     }
 
     // Update is called once per frame
     void Update()
     {
-        gameObject.transform.position = new Vector3(_player.transform.position.x, _player.transform.position.y + 2, _player.transform.position.z - _CameraDistance);
+        // 画面の左上
+        Debug.Log(_camera.ViewportToWorldPoint(new Vector3(0, 0, 12)));
+        Debug.Log(_camera.ViewportToWorldPoint(new Vector3(1, 1, 12)));
+
+        Vector3 currentPos = gameObject.transform.position;
+        gameObject.transform.position = new Vector3(_player.transform.position.x, _player.transform.position.y + 2f, _player.transform.position.z - _CameraDistance);
+        if (_camera.ViewportToWorldPoint(new Vector3(0, 0, 12)).x <= _playerPhisics._leftTop.x)
+        {
+            gameObject.transform.position = new Vector3(currentPos.x, gameObject.transform.position.y, gameObject.transform.position.z);
+        }
     }
 }
